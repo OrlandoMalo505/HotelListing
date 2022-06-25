2974454bc80c87fed5156a4235badb37c7f28a01
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-06-25 15:17:09 +0200

    added serilog support

--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+    path: "C:\\HotelListing\\logs\\log-.txt",
+    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+    rollingInterval: RollingInterval.Day,
+    restrictedToMinimumLevel: LogEventLevel.Information)
+    .CreateLogger();
+
+try
+{
+    Log.Information("App is starting1111111!");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "App failed to start!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
 
 var app = builder.Build();
 
@@ -23,3 +49,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
