cc5fc30311dc9a3e2cd21de07d99304c08c02303
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-07-27 22:34:19 +0200

    added register endpoint

---
+++ HotelListing/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<ApiUser> userManager,
+            ILogger<AccountController> logger, IMapper mapper)
+        {
+            _userManager = userManager;  
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Registration attempt for {userDTO.Email}");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var user = _mapper.Map<ApiUser>(userDTO);
+                user.UserName = userDTO.Email;
+                var result = await _userManager.CreateAsync(user, userDTO.Password);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(Register)}");
+                return Problem($"Something went wrongin the {nameof(Register)}", statusCode: 500);
+            }
+
+            return Accepted();
+        }
+
+        
+    }
+}
---
+++ HotelListing/Dtos/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Dtos
+{
+
+    public class LoginUserDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(15, ErrorMessage = "Password length is not right.")]
+        public string Password { get; set; }
+    }
+
+    public class UserDTO : LoginUserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        
+        
+    }
+}
