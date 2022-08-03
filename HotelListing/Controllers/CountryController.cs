a9db26ab663ec18b44449bb2da088bb027bb1dfd
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-08-03 21:54:15 +0200

    added create endpoints

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HotelListing.Data;
 using HotelListing.Dtos;
 using HotelListing.IRepository;
 using Microsoft.AspNetCore.Http;
@@ -40,7 +41,7 @@ namespace HotelListing.Controllers
             }
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCountry")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetCountry(int id)
@@ -56,6 +57,34 @@ namespace HotelListing.Controllers
                 _logger.LogError(ex, $"Something went wrong in {nameof(GetCountry)}");
                 return StatusCode(500, "Internal server error. Please try again later.");
 
+            }
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateCountry([FromBody] CreateCountryDTO createCountryDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                _logger.LogError($"Invalid POST attempt in {nameof(CreateCountry)}");
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var country = _mapper.Map<Country>(createCountryDTO);
+                await _unitOfWork.Countries.Insert(country);
+                await _unitOfWork.Save();
+
+                return CreatedAtRoute(nameof(GetCountry), new { id = country.CountryId }, country);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in {nameof(CreateCountry)}");
+                return StatusCode(500, "Internal server error. Please try again later.");
             }
         }
     }
--- HotelListing/Controllers/HotelController.cs
+++ HotelListing/Controllers/HotelController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HotelListing.Data;
 using HotelListing.Dtos;
 using HotelListing.IRepository;
 using Microsoft.AspNetCore.Authorization;
@@ -43,7 +44,7 @@ namespace HotelListing.Controllers
         }
 
         [Authorize]
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetHotel")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetHotel(int id)
@@ -59,6 +60,35 @@ namespace HotelListing.Controllers
                 _logger.LogError(ex, $"Something went wrong in {nameof(GetHotel)}");
                 return StatusCode(500, "Internal server error. Please try again later.");
 
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateHotel([FromBody] CreateHotelDTO createHotelDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"Invalid POST attempt in {nameof(CreateHotel)}");
+                return BadRequest(ModelState);    
+            }
+
+            try 
+            {
+                var hotel = _mapper.Map<Hotel>(createHotelDTO);
+                await _unitOfWork.Hotels.Insert(hotel);
+                await _unitOfWork.Save();
+
+                return CreatedAtRoute(nameof(GetHotel), new {id = hotel.HotelId}, hotel);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, $"Something went wrong in {nameof(CreateHotel)}");
+                return StatusCode(500, "Internal server error. Please try again later.");
             }
         }
     }
